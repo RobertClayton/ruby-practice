7a1a66008bfb3de411b77e0e0e43fdf9fd998065
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-05 17:55:42 +0100

    Chapter 3 Complete

---
+++ Chris_Pine/chapter3/chapter3_exercises.rb
@@ -0,0 +1,46 @@
+#chaper 3 exercises
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+puts
+
+puts 'I like' + 'apple pie.'
+
+puts
+
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts
+
+puts 'blink ' * 4
+
+puts
+
+puts  12  +  12
+puts '12' + '12'
+puts '12  +  12'
+
+puts
+
+puts 2 * 5 
+puts'2'* 5 
+puts'2 * 5'
+
+puts
+
+# These cause errors:
+#puts '12' + 12
+#puts '2' * '5'
+
+# need \ for symbols
+puts 'You\'re swell!'
+
+puts
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
