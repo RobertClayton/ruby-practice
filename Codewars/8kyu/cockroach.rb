585bf9c1378f0ba7b74e983b723047bd5fa56bca
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-13 23:10:29 +0100

    Completed Cockroach

---
+++ Codewars/8kyu/cockroach.rb
@@ -0,0 +1,11 @@
+#8kyu cockroach
+
+#The cockroach is one of the fastest insects.
+#Write a function which takes its speed in km per hour
+#and returns it in cm per second, rounded down to the integer.
+
+
+def cockroach_speed(s)
+  answer = s*100000/60/60
+  return answer.floor
+end
