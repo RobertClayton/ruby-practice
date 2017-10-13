f152afedf1c1634faa9a1661234250ce890e0a2e
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-13 23:27:30 +0100

    Added cockroach

--- Codewars/8kyu/cockroach.rb
+++ Codewars/8kyu/cockroach.rb
@@ -4,7 +4,6 @@
 #Write a function which takes its speed in km per hour
 #and returns it in cm per second, rounded down to the integer.
 
-
 def cockroach_speed(s)
   answer = s*100000/60/60
   return answer.floor
