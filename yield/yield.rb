b25f269b39b2459a3dab7c43ce5327f6b15ac6d7
Robert Clayton <robbie.c.clayton@gmail.com>
2018-02-27 12:00:13 +0000

    created my own .each method using yield and self

--- yield/yield.rb
+++ yield/yield.rb
@@ -36,3 +36,20 @@ end
 az = join(' ', 'a'..'z')
 print 'alphabet: '
 puts az
+
+class Array
+  def new_each
+    results = []
+    0.upto(self.length - 1) do |i|
+      results << yield(self[i])
+    end
+    results
+  end
+end
+
+p ['a','g','l'].new_each { |x| x + x }
+# This will double each item in the array ('aa', 'gg', 'll')
+p [1, 2, 3].new_each { |x| x + x }
+# ('2', '4', '6')
+p [1, 2, 3].new_each { |x| x * 4 }
+# ('4', '8', '12')
