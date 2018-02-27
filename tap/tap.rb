8ec71c1e28714fddf9ccda8735b8a78d506a39d5
Robert Clayton <robbie.c.clayton@gmail.com>
2018-02-27 15:48:59 +0000

    examples for Tap

--- tap/tap.rb
+++ tap/tap.rb
@@ -5,3 +5,47 @@
 (1..10).to_a                .tap {|x| puts "array: #{x.inspect}"}
 (1..10).select {|x| x%2==0} .tap {|x| puts "evens: #{x.inspect}"}
 (1..10).map { |x| x*x }     .tap {|x| puts "squares: #{x.inspect}"}
+
+
+#### Code Below is theoretical and will no run:
+
+
+
+# TRADITIONAL
+object = SomeClass.new
+object.key = 'value'
+object
+
+# TAPPED
+object_2 = SomeClass.new.tap do |obj|
+  obj.key = 'value'
+end
+
+# CONDENSED
+object_4 = SomeClass.new.tap { |obj| obj.key = 'value' }
+
+
+
+# TRADITIONAL
+object_4 = Model.new
+object_4.save!
+object_4
+
+# TAPPED
+object_5 = Model.new.tap do |model|
+  model.save!
+end
+
+# CONDENSED
+object_6 = Model.new.tap(&:save!)
+
+
+
+
+# TRADITIONAL
+arr = [1, 2, 3]
+arr.reverse!
+arr
+
+# TAPPED CONDENSED
+[1, 2, 3].tap(&:reverse!)
