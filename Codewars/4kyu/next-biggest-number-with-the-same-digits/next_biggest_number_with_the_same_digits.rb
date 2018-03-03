7d4bf1d648b4b516346a0cfcdae9f222a68e1feb
RobertClayton <robbie.c.clayton@gmail.com>
2018-03-03 18:11:59 +0000

    first passing attempt

--- Codewars/4kyu/next-biggest-number-with-the-same-digits/next_biggest_number_with_the_same_digits.rb
+++ Codewars/4kyu/next-biggest-number-with-the-same-digits/next_biggest_number_with_the_same_digits.rb
@@ -1,5 +1,7 @@
 class NextBiggest
   def next_bigger(n)
-    n + 1
+    array = n.to_s.split('')
+    array = array.permutation(array.length).to_a.map(&:join).uniq.sort
+    array[array.index(n.to_s) + 1].to_i
   end
 end
