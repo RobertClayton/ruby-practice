fdc14ef44db60a82e70d1c9fc924e935cee43a35
RobertClayton <robbie.c.clayton@gmail.com>
2018-03-03 13:21:58 +0000

    set up tests

--- Codewars/4kyu/next-biggest-number-with-the-same-digits/spec/next_biggest_number_with_the_same_digits_spec.rb
+++ Codewars/4kyu/next-biggest-number-with-the-same-digits/spec/next_biggest_number_with_the_same_digits_spec.rb
@@ -3,7 +3,23 @@ describe NextBiggest do
     @next_bigger = described_class.new
   end
 
-  it 'should return n + 1' do
-    expect(@next_bigger.next_bigger(1)).to eq(2)
+  it '12 will return 21' do
+    expect(@next_bigger.next_bigger(12)).to eq(21)
+  end
+
+  it '513 will return 531' do
+    expect(@next_bigger.next_bigger(513)).to eq(531)
+  end
+
+  it '2017 will return 2071' do
+    expect(@next_bigger.next_bigger(2017)).to eq(2071)
+  end
+
+  it '414 will return 441' do
+    expect(@next_bigger.next_bigger(414)).to eq(441)
+  end
+
+  it '144 will return 414' do
+    expect(@next_bigger.next_bigger(144)).to eq(414)
   end
 end
